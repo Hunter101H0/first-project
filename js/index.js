b0cc773bdad616116e4678fc6c701e52b097b126
Hunter101H0 <100170708+Hunter101H0@users.noreply.github.com>
2023-01-26 14:53:02 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function change() {
+
+
+var tesr = document.getElementById("hello");
+
+window.location.hash =  tesr;
+window.addEventListener('change', function () {  
+   
+    window.location.hash =  tesr;
+    
+  
+
+})
+
+}
+
